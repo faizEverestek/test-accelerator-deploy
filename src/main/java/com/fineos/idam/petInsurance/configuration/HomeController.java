289a6813fc3cc6050b315ffdff752736d062cff4
Faiz Qureshi <faiz.qureshi@everestek.com>
2025-07-19 19:42:28 +0530

    Initial commit

---
+++ src/main/java/com/fineos/idam/petInsurance/configuration/HomeController.java
@@ -0,0 +1,29 @@
+/*---------------------------------------------------------------------------------------------
+FINEOS
+
+(c) Copyright FINEOS.
+ALL RIGHTS RESERVED
+
+---------------------------------------------------------------------------------------------*/
+package com.fineos.idam.petInsurance.configuration;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/** Home redirection to OpenAPI api documentation. */
+@Controller
+public class HomeController {
+
+    /**
+     * Handles HTTP GET requests for the root URL ("/").
+     *
+     * <p>This method redirects the client to the Swagger UI page. When a user navigates to the root of the application,
+     * they are automatically redirected to the Swagger documentation page located at "swagger-ui.html".
+     *
+     * @return a string representing the redirection URL to the Swagger UI page.
+     */
+    @GetMapping("/")
+    public String index() {
+        return "redirect:swagger-ui.html";
+    }
+}
